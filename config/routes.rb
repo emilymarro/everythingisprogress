a6dc2a21f5e1d2544154d61bb8552ab43b1a5d34
Emily Marro <emily.marro@gmail.com>
2018-02-27 10:19:04 -0500

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  devise_for :users
+  # devise_for :users
+
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # devise_for :admins, path: 'admin', skip: :registrations
   root to: "home#index"
@@ -17,3 +25,4 @@ Rails.application.routes.draw do
   resources :users
   resources :instructions
 end
+
