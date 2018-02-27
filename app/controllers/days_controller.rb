f2eb68faa97c772fddaed5ac388abe65d98556fa
Emily Marro <emily.marro@gmail.com>
2018-02-27 11:36:00 -0500

    sort dates by asc

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,10 +1,13 @@
 class DaysController < ApplicationController
   before_action :authenticate_user!
   def index
-    @days = Day.where(user_id: current_user.id)
+    @days = Day.where(user_id: current_user.id).order("date ASC").all
     # @activities = Activity.where(user)
     puts current_user.id
   end
+
+
+
 
   def show
     @day = Day.find(params[:id])
